bbaa4d80164617e99dab002397157b52f29f5e27
StrixV54 <64365738+StrixV54@users.noreply.github.com>
2021-11-22 20:36:16 +0530

    fixes - responsiveness

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const navbar = document.querySelector('#navbar');
 const navlogo = document.querySelector('.logo');
+const navmenu = document.querySelector('.menu-icon');
 let navTop = navbar.offsetTop+30;
-console.log(navTop);
+// console.log(navTop);
 function fixedNav() {
   if (window.scrollY >= navTop) {
     navbar.classList.add('fixed');
@@ -14,3 +15,19 @@ function fixedNav() {
 document.addEventListener('DOMContentLoaded', function() {
     window.addEventListener('scroll', fixedNav);
 })
+navmenu.addEventListener("click",()=>{
+  let item=document.querySelector("header ul");
+  // console.log("-> "+item);
+  if(item.style.display==="none"){
+    item.style.display="block";
+    document.querySelector("header").style.background="rgba(39, 37, 75, 0.8)";
+    document.querySelector("header").style.height="100vh";
+    setTimeout(function(){navmenu.style.transform = 'rotate(90deg)';},100);
+  }
+  else{
+    item.style.display="none";
+    document.querySelector("header").style.background="none";
+    document.querySelector("header").style.height="80px";
+    setTimeout(function(){navmenu.style.transform = 'rotate(0deg)';},100);
+  }
+});
