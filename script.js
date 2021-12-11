ff3203ebdd1aa866e183c48eebe26f127c439a56
StrixV54 <64365738+StrixV54@users.noreply.github.com>
2021-12-11 15:22:10 +0530

    Improvement fixes

--- script.js
+++ script.js
@@ -1,33 +1,37 @@
 const navbar = document.querySelector('#navbar');
 const navlogo = document.querySelector('.logo');
 const navmenu = document.querySelector('.menu-icon');
+const navlink = document.querySelectorAll('.menu-a');
+let item=document.querySelector("header ul");
+
 let navTop = navbar.offsetTop+30;
 // console.log(navTop);
 function fixedNav() {
-  if (window.scrollY >= navTop) {
+  if (window.scrollY >= navTop ) {
     navbar.classList.add('fixed');
     navlogo.classList.add('show');
   } else {
     navbar.classList.remove('fixed');    
-    navlogo.classList.remove('show');    
+    navlogo.classList.remove('show'); 
   }
 }
 document.addEventListener('DOMContentLoaded', function() {
     window.addEventListener('scroll', fixedNav);
-})
+});
+for (let i = 0; i < navlink.length; i++){
+  navlink[i].addEventListener("click",function(e){
+    item.style.display="none";
+    navmenu.classList.remove('show');
+  });
+}
 navmenu.addEventListener("click",()=>{
-  let item=document.querySelector("header ul");
   // console.log("-> "+item);
   if(item.style.display==="none"){
     item.style.display="block";
-    document.querySelector("header").style.background="rgba(39, 37, 75, 0.8)";
-    document.querySelector("header").style.height="100vh";
-    setTimeout(function(){navmenu.style.transform = 'rotate(90deg)';},100);
+    navmenu.classList.add('show');
   }
   else{
     item.style.display="none";
-    document.querySelector("header").style.background="none";
-    document.querySelector("header").style.height="80px";
-    setTimeout(function(){navmenu.style.transform = 'rotate(0deg)';},100);
+    navmenu.classList.remove('show');
   }
 });
