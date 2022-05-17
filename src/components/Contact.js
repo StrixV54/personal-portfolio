71b039ea410bca15907dcbe7dda5401cdea9559a
Shrikant Vishwakarma <shrikantvk54@gmail.com>
2022-05-17 09:21:32 +0530

    minor changes

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -10,10 +10,10 @@ function Contact(){
             <img src="/assets/thinking.svg"/>
             <div>
                 <span className="bold">Reach Me on gmail</span>
-                Email: shrikantvk54@gmail.com
+                Email: shrikantkumar54@gmail.com
                 <span className="bold">or</span> 
                 <span className="bold">Call me anytime</span>
-                Contact No: 9910283831
+                Contact No: 99XXXXXXXX
                 <div className="social-bar">
                     <i className="fab fa-github fa-3x" href=""></i>
                     <i className="fab fa-twitter-square fa-3x"></i>
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -8,16 +8,28 @@ function Skills(){
         <div id="Skills" className="section" data-aos="fade-up">
             <p className="headline">Skills</p>
             <ul data-aos="fade-up">
-                <li><i className="fab fa-java fa-4x"></i> Java</li>
-                <li><i className="fas fa-copyright fa-4x"></i> C++</li>
-                <li><i className="fab fa-python fa-4x"></i> Python</li>
-                <li><i className="fas fa-ad fa-4x"></i> Photoshop</li>
-                <li><i className="fab fa-html5 fa-4x"></i> HTML5</li>
-                <li><i className="fab fa-css3 fa-4x"></i> CSS3</li>
-                <li><i className="fab fa-js fa-4x"></i> Javascript</li>
-                <li><i className="fab fa-react fa-4x"></i> React</li>
+                {/* <li><i className="fab fa-java fa-4x"></i> Java</li> */}
+                <li><img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg" /> Java</li>
+                {/* <li><i className="fas fa-copyright fa-4x"></i> C++</li> */}
+                <li><img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/cplusplus/cplusplus-original.svg" />C++</li>
+                {/* <li><i className="fab fa-python fa-4x"></i> Python</li> */}
+                <li><img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg" />Python</li>
+                {/* <li><i className="fas fa-ad fa-4x"></i> Photoshop</li> */}
+                <li><img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/photoshop/photoshop-plain.svg" />Photoshop</li>
+                {/* <li><i className="fab fa-html5 fa-4x"></i> HTML5</li> */}
+                <li><img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" />HTML</li>
+                {/* <li><i className="fab fa-css3 fa-4x"></i> CSS3</li> */}
+                <li><img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" />CSS</li>
+                {/* <li><i className="fab fa-js fa-4x"></i> JavaScript</li> */}
+                <li><img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-plain.svg" /> JavaScript</li>
+                {/* <li><i className="fab fa-react fa-4x"></i> React</li> */}
+                <li><img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg" />NodeJs</li>
+                <li><img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" />React</li>
+                <li><img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg" /> Git</li>
+                <li><img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/googlecloud/googlecloud-original.svg" />Google Cloud</li>
+                <li><img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/intellij/intellij-original.svg" />IntelliJ</li>
             </ul>
-            <img src="/assets/design.svg"/>
+            <img className="side-img" src="/assets/design.svg"/>
         </div>
         </Fade>
     )
